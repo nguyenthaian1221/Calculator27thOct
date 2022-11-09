a106b35af17fd3bfd17d79583c6ae7997040912a
annt <annt@rocketstudio.com.vn>
2022-11-09 21:22:29 +0700

    btvn

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -15,14 +15,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        TextView tv = findViewById(R.id.textviewCuaAn);
-
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"Quá tuyệt vời luôn",Toast.LENGTH_LONG).show();
-            }
-        });
-
     }
 }
